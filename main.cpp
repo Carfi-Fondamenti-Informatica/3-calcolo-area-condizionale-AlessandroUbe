b08ae5e25c130ebfdbe19538450532a4c0a99d6f
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-11-20 17:28:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+  
+   float a=0;
+   float b=0;
+   int opzione=0;
+
+    cin>>a;
+    cin>>b;
+    cin>>opzione;
+
+    switch (opzione) {
+        case 0: cout<<(a*b)/2;
+        break;
+        case 1: cout<<(a*a);
+        break;
+        case 2: cout<<(a*b);
+        break;
+        default:
+            cout<<"stringa non valida";
+    }
+   
+   
+   
    return 0;
 }
